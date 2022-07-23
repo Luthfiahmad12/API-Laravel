6588b8cbd085c669151075b55f31ece6efc3775c
luthfiafif625@gmail.com <luthfiafif625@gmail.com>
2022-07-23 15:59:11 +0700

    update

--- resources/views/homepage/index.blade.php
+++ resources/views/homepage/index.blade.php
@@ -1,16 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.template')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Data Mahasiswa MI</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
-</head>
-
-<body style="background: indigo">
+@section('content')
     <div style="padding-buttom:20px" class="d-grid gap-2 d-md-flex justify-content-md-end">
         <a href="/admin" class="btn btn-success me-md-2" type="button"><b> Halaman Admin</b></a>
     </div>
@@ -48,24 +38,4 @@
             </div>
         </div>
     </div>
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
-
-    {{-- <script>
-        //message with toastr
-        @if (session()->has('success'))
-            ;
-
-            toastr.success('{{ session('success') }}', 'BERHASIL!');
-        @elseif
-            (session()->has('error'));
-
-            toastr.error('{{ session('error') }}', 'GAGAL!');
-        @endif
-    </script> --}}
-
-</body>
-
-</html>
+@endsection
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <hr />
+            <p class="text-center" style="color: white"><strong>&copy Copyright LoodDev.</strong> All rights reserved.
+        </div>
+    </div>
+</div>
